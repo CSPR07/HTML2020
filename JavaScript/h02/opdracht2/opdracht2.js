3cecce65098870e275225d48fb78935c610402c1
Caspar van Vuure <2140608@talnet.nl>
2020-11-05 15:22:56 +0100

    hoofdstuk 2 opdracht 2 volledig klaar met de javascript element background

---
+++ JavaScript/h02/opdracht2/opdracht2.js
@@ -0,0 +1,4 @@
+document.getElementById("bluesquare").style.backgroundColor = "blue";
+document.getElementById("greensquare").style.backgroundColor = "green";
+document.getElementById("yellowsquare").style.backgroundColor = "yellow";
+document.getElementById("redsquare").style.backgroundColor = "red";
