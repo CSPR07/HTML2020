0c185b98fc0c23a041d9422859272419f999e73c
Caspar van Vuure <2140608@talnet.nl>
2020-11-05 13:57:31 +0100

    hoofdstuk 1 af en hoofdstuk 2 opdracht 1 klaar

--- JavaScript/h01/opdracht1/opdracht1.js
+++ JavaScript/h01/opdracht1/opdracht1.js
@@ -1,4 +1,17 @@
-var a = "dit is een tekst";
-if(a > 9){
-    console.log("Hallo a is groter dan 9")
+
+console.log("Hello World");
+
+//**********************************//
+for (var i = 0; i < 10; i++){
+    console.log("De waarde van i is "+ i);
 }
+
+//**********************************//
+
+var mijnauto = {
+    merk:"Volkswagen",
+    type:"golf",
+    aantalWielen: 4,
+    kleur:"wit"
+}
+console.log(mijnauto.kleur);
---
+++ JavaScript/h02/opdracht1/opdracht1.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "mijn geweldige pagina";
+document.getElementById("subkop").innerText = "mijn geweldige subkop";
+document.getElementById("link").src="8bitlink.png";
+document.getElementById("hawaii").src="hawaii256pix.png";
